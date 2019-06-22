ffa19e8fd0a0b5cd797ef85a0b9b60c46b132e44
Karla Gallegos <kgalleg618@gmail.com>
2019-06-22 16:01:13 -0500

    finished journal entry 3

--- scripts/journal.js
+++ scripts/journal.js
@@ -3,28 +3,67 @@
     represents a journal entry about what you learned today
 */
 
-let journalEntries=[]
-const journalEntry1 = {  
-    date: "June 3, 2019",
-    conceptCovered: "Loops and objects",
-    longContent: "Today we went over objects which are containeres for data.",
-    mood: "excited"
-}
-const journalEntry2 = {
-    date: "June 3, 2019",
-    conceptCovered: "concept covered goes here",
-    longContent: "Today we went over objects which are containeres for data.",
-    mood: "excited"
-}
-const journalEntry3 = {
-    date: "June 5, 2019",
-    conceptCovered: "Loops and objects",
-    longContent: "Today we went over other stuff.",
-    mood: "mind-blown"
-}
+let journalEntries=[
+{  
+    dateOfentry: "June 3, 2019",
+    conceptOfentry: "Loops and objects",
+    entryForToday: "Today we went over objects which are containeres for data.",
+    moodForToday: "excited"
+},
+ {
+    dateOfentry: "June 4, 2019",
+    conceptOfentry: "concept covered goes here",
+    entryForToday: "Today we went over objects which are containeres for data.",
+    moodForToday: "excited"
+},
+ {
+    dateOfentry: "June 5, 2019",
+    conceptOfentry: "Loops and objects",
+    entryForToday: "Today we went over other stuff.",
+    moodForToday: "mind-blown"
+}]
 
-journalEntries.push(journalEntry1, journalEntry2, journalEntry3);
+// journalEntries.push(journalEntry1, journalEntry2, journalEntry3);
 // you put the container that the stuff is going into, so here the entries will go into the journalentries array
 // make sure to go to the server in the html File, if not ,it will not Work.
 
 console.log(journalEntries);
+
+
+const makeJournalEntryComponent = (journalEntry) => {
+    // Create your own HTML structure for a journal entry
+    return ` 
+    <div class="journalEntries">
+                <h1>${journalEntry.dateOfentry}</h1>
+                <section>${journalEntry.conceptOfentry}</section>
+                <aside>${journalEntry.entryForToday}</aside>
+                <aside>${journalEntry.moodForToday}</aside>
+            </div>
+    `
+}
+// console.log(makeJournalEntryComponent);
+
+
+
+
+// Now write a function whose reponsibility is to iterate your array of journal entries and add them to the DOM.
+
+// src/scripts/journal.js
+
+/*
+    Purpose: To render all journal entries to the DOM
+
+    Arguments: entries (array of objects)
+*/
+const renderJournalEntries = (entries) => {
+    for (let i = 0; i < entries.length; i++) {
+        document.querySelector(".entryLog").innerHTML += makeJournalEntryComponent (
+          entries[i]
+        )
+    }
+
+}
+
+// Invoke the render function
+renderJournalEntries(journalEntries);
+
